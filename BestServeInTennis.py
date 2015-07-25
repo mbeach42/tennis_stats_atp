fc35249d0a0a5d2704570123bcd3b5f21d153491
mbeach42 <all.guitars.for.me@gmail.com>
2015-07-25 11:34:29 -0700

    modularized the serving file
    also deleted some figures

--- BestServeInTennis.py
+++ BestServeInTennis.py
@@ -1,79 +1,102 @@
 # Tennis Men's Singles ATP "Who has the best aces?"
+# cleaner code
 import pandas as pd
 import numpy as np
-
-dirname = 'C:\\Users\\Me\\Documents\\GitHub\\tennis_stats_atp\\'
-year = '2006'
-Surf = 'Hard'
-cut_off = 0.0
-b = 4
-
-df = pd.read_csv(dirname+'atp_matches_'+year+'.csv')
-# drop unimportant columns
-df = df[['surface', 'winner_name', 'loser_name', 'w_ace', 'l_ace', 'w_1stIn', 'l_1stWon','l_1stIn', 'l_1stWon','w_SvGms', 'l_SvGms']]
-# select only grass courts
-#df = df[df['surface'].isin([Surf])]
-df = df.drop('surface', 1)
-# only take finite values (ignore NaN)
-df = df.dropna() 
-
-# calculate aver ace/game
-df['w_avgAGm'] = 1.0*df['w_ace']/df['w_SvGms']
-df['l_avgAGm'] = 1.0*df['l_ace']/df['l_SvGms']
-
-# split into winners and losers seperatly, then combine into one dataframe
-w_df = df[['winner_name', 'w_avgAGm']]
-w_df.columns = ['name', 'avgAGm'] # rename columns
-l_df = df[['loser_name', 'l_avgAGm']]
-l_df.columns = ['name', 'avgAGm'] # rename columns
-simple_df = pd.concat([w_df, l_df])
-
-# Simply "who has the highers (unweighted) number of aces/game
-grouped = simple_df.groupby('name')
-A = grouped.mean().sort(['avgAGm'])
-print "Simple analysis of the most aces on "+Surf+" courts."
-print A.tail()
-
-# a more complicated weighting using a player/player matrix
-df2 = df[['winner_name', 'loser_name', 'l_avgAGm']]
-gdf = df2.groupby(['winner_name', 'loser_name'], as_index=False).aggregate(np.mean)
-norms = gdf.groupby(['winner_name'], as_index=False).aggregate(np.mean)
-# rename norm
-norms.rename(columns={'winner_name': 'name', 'l_avgAGm': 'N'}, inplace=True)
-#exclude singular terms
-norms = norms[norms['N'] > cut_off].sort('N')
-
-df3 = df[['winner_name', 'loser_name', 'w_avgAGm']]
-acedf = df3.groupby(['winner_name', 'loser_name'], as_index=False).aggregate(np.mean)
-acedf.rename(columns={'loser_name': 'name'}, inplace=True)
-# Join the two things
-norms = norms.set_index('name')
-master_df = acedf.join(norms, on='name')
-master_df = master_df.dropna() 
-#remove nans
-
-#Kinds of functions
-master_df['weight']  = np.exp(-1.0/2/b**2*(master_df['w_avgAGm']-master_df['N'])**2)
-master_df['weight']  = (master_df['w_avgAGm']-master_df['N'])
-master_df = master_df[['winner_name', 'weight']]
-master_df.rename(columns={'weight': 'ace_score'}, inplace=True)
-master_df = master_df.groupby(['winner_name'], as_index=False).aggregate(np.sum)
-master_df = master_df.set_index('winner_name')
-master_df = master_df.sort('ace_score')
-#print norms.loc['Roger Federer']
-print master_df
-
-#top 20
-top20_df = master_df.tail(20)
-
-#plotting the data
 import matplotlib
 import matplotlib.pyplot as plt
-top20_df.plot(kind = 'bar', legend=False, title = "Ace Score of ATP Men's Singles Players in "+year)
-plt.grid(b = False)
-plt.xlabel('Player')
-plt.ylabel('Ace Score')
+    
+dirname = 'C:\\Users\\Me\\Documents\\GitHub\\tennis_stats_atp\\'
+year = '2007'
+Surf = 'Hard'
 
+# import data, only take important columns, and drop missing data
+def readData(year, Surf):
+    df = pd.read_csv(dirname+'atp_matches_'+year+'.csv')
+    df = df[['surface', 'winner_name', 'loser_name', 'w_ace', 'l_ace', 'w_1stIn', 'l_1stWon','l_1stIn', 'l_1stWon','w_SvGms', 'l_SvGms']]
+    if Surf != 'All':
+        df = df[df['surface'].isin([Surf])]
+    df = df.drop('surface', 1)
+    df = df.dropna() 
+    return df
 
+# average number of aces per game
+def avgAcePerGm(df):
+    df['w_avgAGm'] = 1.0*df['w_ace']/df['w_SvGms']
+    df['l_avgAGm'] = 1.0*df['l_ace']/df['l_SvGms']
+    return df
+    
+def unweightedScore(df):
+    # split into winners and losers seperatly, then combine into one dataframe
+    w_df = df[['winner_name', 'w_avgAGm']]
+    w_df.columns = ['name', 'avgAGm'] # rename columns
+    l_df = df[['loser_name', 'l_avgAGm']]
+    l_df.columns = ['name', 'avgAGm'] # rename columns
+    simple_df = pd.concat([w_df, l_df])  
+    # Simply "who has the highers (unweighted) number of aces/game
+    score_df = simple_df.groupby('name').mean().sort(['avgAGm'])
+    print "Simple analysis of the most aces on "+Surf+" courts."
+    print score_df.tail()
+    return score_df
 
-plt.savefig('figures\\'+year+'.png')
+# More advanced functions
+def normalizePlayers(df):
+    gdf = df[['winner_name', 'loser_name', 'l_avgAGm']] \
+        .groupby(['winner_name', 'loser_name'], as_index=False) \
+        .mean()
+    norms = df2.groupby(['winner_name'], as_index=False).mean()
+    norms.rename(columns={'winner_name': 'name', 'l_avgAGm': 'N'}, inplace=True)
+    norms = norms.set_index('name')
+    norms = norms.dropna() 
+    return norms
+
+def acesDF(df):
+    ace_df = df[['winner_name', 'loser_name', 'w_avgAGm']] \
+        .groupby(['winner_name', 'loser_name'], as_index=False) \
+        .mean()
+    ace_df.rename(columns={'loser_name': 'name'}, inplace=True)
+    # Join the norms and scores_df
+    norms = normalizePlayers(df)
+    joined_df = ace_df.join(norms, on='name')
+    joined_df = joined_df.dropna() 
+    return joined_df
+    
+def weightedScore(aces_df):
+    scores_df = aces_df
+    scores_df['ace_score']  = aces_df['w_avgAGm'] - aces_df['N']
+    scores_df_mean = scores_df[['winner_name', 'ace_score']] \
+        .groupby(['winner_name']) \
+        .mean()
+    scores_df_std = scores_df[['winner_name', 'ace_score']] \
+        .groupby(['winner_name']) \
+        .std()
+    scores_df = scores_df_mean
+    scores_df['std'] = scores_df_std['ace_score']
+    scores_df = scores_df.replace([np.inf, -np.inf], np.nan).dropna()
+    scores_df = scores_df.sort('ace_score')
+    scores_df.rename(columns={'winner_name': 'name'}, inplace=True)
+    return scores_df
+
+def plotTopN(df, N):
+    top_df = df.tail(N)
+    top_df['ace_score'].plot(kind = 'bar', 
+        yerr=top_df[['std']].values.T,
+        legend=False,
+        title = "Ace Score of ATP Men's Singles Players in "+year+" on "+Surf)
+    plt.grid(b = False)
+    plt.xlabel('Player')
+    plt.ylabel('Ace Score')
+    plt.savefig('figures\\'+year+'_'+Surf+'.png')
+
+# Simple scores
+df = readData(year, Surf)
+df = avgAcePerGm(df)
+unweightedScore(df)
+
+# Weighted scores
+df = readData(year, Surf)
+df = avgAcePerGm(df)
+aces_df = acesDF(df)
+scores_df = weightedScore(aces_df)
+print scores_df.tail(4)
+
+plotTopN(scores_df, 50)
